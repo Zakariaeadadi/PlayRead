5c2796f3c40948f7afc9972bb595f60a9df83871
Zakariaeadadi <zakariaeadadi01@gmail.com>
2026-02-12 23:01:48 +0000

    first commit

---
+++ src/GameInfo.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+export default function GameInfo({ game }){
+    return (
+        <>
+                <div className='game-card'>
+                    {/* CONTENT */}
+                    <div className='game-content'>
+                        <img className='img' src={game.img}/>
+
+                        <div className='game-text'>
+                            <h2>{game.name}</h2>
+                            <p>{game.description}</p>
+                            <p>genre : {game.genre} || platform : {game.platform}</p>
+                        </div>
+                    </div>
+                    <div className='game-btn'>
+                        <a target='target-blank' href={game.url}><button className='btn' style={{background: "blueviolet", width: "125px"}}>Click</button></a>
+                    </div>
+                    {/*======= CONTENT ======*/}
+
+                </div>
+        </>
+    )
+}
